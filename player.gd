e6b11bca639c561814f1fd9fecddf45437f0991e
Long Vo <longvo.work84@gmail.com>
2026-02-21 21:57:58 -0600

    wip new qte system

--- player.gd
+++ player.gd
@@ -14,32 +14,18 @@ func _physics_process(delta: float) -> void:
 	var velocity = direction * speed
 	move_and_slide()
 
-
-func _process(delta):
-	# quick time event
-	if qte_active:
-		qte_time -= delta
-		
-		if Input.is_action_just_pressed("qte_action") and qte_target: # handles killing enemies
-			print("Enemy killed.")
-			qte_target.queue_free()
-			qte_active = false
-			qte_target = null
-		
-		if qte_time <= 0: # failed qte
-			print("QTE failed.")
-			qte_active = false
-			qte_target = null
+signal qte_triggered(enemy)
 
 func _on_detection_area_body_entered(body: Node2D) -> void:
-	if body.is_in_group("enemies") and not qte_active: # checks enemy in area
-		print("Enemy detected.")
+	if body.is_in_group("enemies") and not qte_active:
 		qte_active = true
 		qte_target = body
-		qte_time = qte_duration
-		print("QTE triggered.")
+		emit_signal("qte_triggered", body)
 	
-func _on_interaction_area_body_entered(body: Node2D) -> void:
-	if body.is_ingroup("items"): # detects nearby items
+func _on_interaction_area_body_entered(body: Node2D) -> void: # detects nearby items
+	if body.is_ingroup("items"):
 		nearby_item = body
 		print("Item in range: ", body.name)
+
+func take_damage():
+	print("Player damaged")
