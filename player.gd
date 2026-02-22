3f2bf948f1ddf900fdf16c49dc3058dd84fa2457
SoggeToast <miguel.123.canales@gmail.com>
2026-02-22 01:45:43 -0600

    mapping and lighting played with

--- player.gd
+++ player.gd
@@ -1,18 +1,33 @@
 extends CharacterBody2D
 
-@onready var health_ui = get_tree().current_scene.get_node("UI/HealthUI")
-
 var speed: float = 600
-var nearby_item: Node = null 
-var health = 3
 
-func _physics_process(delta: float) -> void:
+var nearby_item: Node = null 
+@onready var anim = $AnimatedSprite2D
+
+#const FLOATING_TEXT = preload("res://floating_text.tscn") 	
+
+func _physics_process(_delta: float) -> void:
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	var velocity = direction * speed
+	velocity = direction * speed
 	move_and_slide()
 	
+	#animation
+	if direction.x > 0:
+		anim.play("move right")
+	elif direction.x < 0:
+		anim.play("move left")
+	elif direction.y > 0:
+		anim.play("move down")
+	elif direction.y < 0:
+		anim.play("move up")
+	else:
+		# Stops playing when you release the keys
+		anim.stop()
+	
 func _on_interaction_area_body_entered(body: Node2D) -> void:
-	if body.is_ingroup("items"): # detects nearby items
+
+	if body.is_in_group("items"): # detects nearby items
 		nearby_item = body
 		print("Item in range: ", body.name)
 
@@ -24,9 +39,12 @@ func _on_interaction_area_body_exited(body: Node2D) -> void:
 func _input(event):
 	if event.is_action_pressed("interact") and nearby_item: # interact with item
 		print("Interacted with: ", nearby_item.name)
+		
+		#var text_instance = FLOATING_TEXT.instantiate()
+		#text_instance.text = "wha da helly...." # You can change what it says here!
+		#text_instance.global_position = nearby_item.global_position # Spawn it at the item
+		#get_tree().current_scene.add_child(text_instance) # Add it to the game world
+		
 		nearby_item.queue_free()
 		nearby_item = null
-
-func take_damage(): # take 1 damage from monster
-	health -= 1
-	health_ui.set_health(health)
+		get_node("../CanvasLayer/Control").collect_fuel()
